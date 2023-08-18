1282849bbd3423220d93fe68d217d823991491f7
Matthieu-Nzinga <111420754+Matthieu-Nzinga@users.noreply.github.com>
2023-08-18 19:51:10 +0000

    Update configuration

--- script.js
+++ script.js
@@ -1,189 +1,189 @@
-// { calculate } import './calculator';
+
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 
 
 const ac = document.getElementById('reset');
-  const c = document.getElementById('clear');
-  const plusOuMoins = document.getElementById('plusoumoins');
-  const pourcentage = document.getElementById('percentage');
-  const division = document.getElementById('divideby');
-  const multiplication = document.getElementById('times');
-  const soustraction = document.getElementById('minus');
-  const addition = document.getElementById('plus');
-  const egalites = document.getElementById('equals');
+const c = document.getElementById('clear');
+const plusOuMoins = document.getElementById('plusoumoins');
+const pourcentage = document.getElementById('percentage');
+const division = document.getElementById('divideby');
+const multiplication = document.getElementById('times');
+const soustraction = document.getElementById('minus');
+const addition = document.getElementById('plus');
+const egalites = document.getElementById('equals');
 
-  const listButtons = document.getElementsByClassName("digit");
-  const buttonpoint = document.querySelector(".dot");
+const listButtons = document.getElementsByClassName("digit");
+const buttonpoint = document.querySelector(".dot");
 
-  const inputAffichager = document.getElementById("input");
-  const labelHistoryque = document.getElementById("calcul");
+const inputAffichager = document.getElementById("input");
+const labelHistoryque = document.getElementById("calcul");
 
-  let numeroCourant = "";
-  let operateurCourant = "";
-  let calculEnCours = "";
-  let operateurPresse = false;
- 
+let numeroCourant = "";
+let operateurCourant = "";
+let calculEnCours = "";
+let operateurPresse = false;
 
 
-  window.addEventListener("load", () => {
-    inputAffichager.value = '0';
+
+window.addEventListener("load", () => {
+  inputAffichager.value = '0';
+});
+
+window.addEventListener("keydown", function(event) {
+  event.preventDefault();
+
+});
+
+for (let i = 0; i < listButtons.length; i++) {
+  listButtons[i].addEventListener("click", function() {
+    operateurPresse = false;
+    const element = listButtons[i].textContent.trim();
+    const lab = document.getElementById("calcul");
+
+    if (lab.innerHTML.includes("=")) {
+      console.log(lab.innerHTML);
+      inputAffichager.value = element;
+      labelHistoryque.innerHTML = "";
+      numeroCourant = element;
+      return;
+    }
+
+    if (numeroCourant === "0") {
+      numeroCourant = element
+    } else {
+      numeroCourant += element;
+    }
+
+    inputAffichager.value = numeroCourant;
   });
+}
 
-  window.addEventListener("keydown", function (event) {
-    event.preventDefault();
-    
-  });
+buttonpoint.addEventListener("click", function() {
+  if (!numeroCourant.includes(".")) {
 
-  for (let i = 0; i < listButtons.length; i++) {
-    listButtons[i].addEventListener("click", function () {
-      operateurPresse = false;
-      const element = listButtons[i].textContent.trim();
-      const lab = document.getElementById("calcul");
-      
-      if(lab.innerHTML.includes("=")){
-        console.log(lab.innerHTML);
-        inputAffichager.value = element;
-        labelHistoryque.innerHTML = "";
-        numeroCourant = element;
-        return;
-      }
-
-      if (numeroCourant === "0") {
-        numeroCourant = element
-      } else {
-        numeroCourant += element;
-      }
-
-      inputAffichager.value = numeroCourant;
-    });
+    numeroCourant += ".";
+    inputAffichager.value = numeroCourant;
   }
+});
 
-  buttonpoint.addEventListener("click", function () {
-    if (!numeroCourant.includes(".")) {
+plusOuMoins.addEventListener("click", function() {
 
-      numeroCourant += ".";
-      inputAffichager.value = numeroCourant;
-    }
-  });
+  numeroCourant = (parseFloat(numeroCourant) * -1).toString();
+  inputAffichager.value = numeroCourant;
+});
 
-  plusOuMoins.addEventListener("click", function () {
+pourcentage.addEventListener("click", function(event) {
+  event.preventDefault();
 
-    numeroCourant = (parseFloat(numeroCourant) * -1).toString();
-    inputAffichager.value = numeroCourant;
-  });
+  numeroCourant = (parseFloat(numeroCourant) / 100).toString();
+  inputAffichager.value = numeroCourant;
+  labelHistoryque.textContent = numeroCourant;
+});
 
-  pourcentage.addEventListener("click", function (event) {
-    event.preventDefault();
+c.addEventListener("click", function(event) {
+  event.preventDefault();
+  // numeroCourant = "";
+  // operateurCourant = "";
+  // calculEnCours = "";
+  // inputAffichager.value = 0;
 
-    numeroCourant = (parseFloat(numeroCourant) / 100).toString();
-    inputAffichager.value = numeroCourant;
-    labelHistoryque.textContent = numeroCourant;
-  });
-
-  c.addEventListener("click", function (event) {
-    event.preventDefault();
-    // numeroCourant = "";
-    // operateurCourant = "";
-    // calculEnCours = "";
-    // inputAffichager.value = 0;
-
-    if (numeroCourant.length > 0) {
-      numeroCourant = numeroCourant.slice(0, -10)
-      inputAffichager.value = 0;
-    }
-  });
-
-  ac.addEventListener("click", function (event) {
-    event.preventDefault();
-    numeroCourant = "";
-    operateurCourant = "";
-    calculEnCours = "";
+  if (numeroCourant.length > 0) {
+    numeroCourant = numeroCourant.slice(0, -10)
     inputAffichager.value = 0;
-    labelHistoryque.textContent = "";
-    
-  });
+  }
+});
 
-  
+ac.addEventListener("click", function(event) {
+  event.preventDefault();
+  numeroCourant = "";
+  operateurCourant = "";
+  calculEnCours = "";
+  inputAffichager.value = 0;
+  labelHistoryque.textContent = "";
 
-  function operateurs(newOperator) {
-    if (numeroCourant !== "" && operateurPresse==false) {
-      operateurCourant = newOperator;
-      calculEnCours += " " + numeroCourant + " " + operateurCourant;
-      labelHistoryque.textContent = calculEnCours;
-      operateurPresse = true;
-      numeroCourant = "";
+});
+
+
+
+function operateurs(newOperator) {
+  if (numeroCourant !== "" && operateurPresse == false) {
+    operateurCourant = newOperator;
+    calculEnCours += " " + numeroCourant + " " + operateurCourant;
+    labelHistoryque.textContent = calculEnCours;
+    operateurPresse = true;
+    numeroCourant = "";
+  }
+  else {
+    labelHistoryque.textContent = labelHistoryque.textContent.slice(0, -1) + ' ' + newOperator;
+    calculEnCours = calculEnCours.slice(0, -1) + ' ' + newOperator;
+  }
+}
+
+
+// function operateurs(newOperator) {
+//   if (numeroCourant !== "") {
+//     operateurCourant = newOperator;
+//     calculEnCours += " " + numeroCourant + " " + operateurCourant;
+//     labelHistoryque.textContent = calculEnCours;
+//     numeroCourant = "";
+//   }
+// }
+
+division.addEventListener("click", function(event) {
+  event.preventDefault();
+  operateurs("/");
+});
+
+multiplication.addEventListener("click", function(event) {
+  event.preventDefault();
+  operateurs("*");
+});
+
+soustraction.addEventListener("click", function(event) {
+  event.preventDefault();
+  operateurs("-");
+});
+
+addition.addEventListener("click", function(event) {
+  event.preventDefault();
+  operateurs("+");
+});
+
+egalites.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (numeroCourant !== "" && operateurCourant !== "") {
+    calculEnCours += numeroCourant;
+    labelHistoryque.textContent = calculEnCours + " = ";
+    let resultat;
+
+    switch (operateurCourant) {
+      case "+":
+      case "-":
+      case "*":
+        resultat = eval(calculEnCours);
+        break;
+      case "/":
+        if (numeroCourant !== "0") {
+          resultat = eval(calculEnCours);
+        } else {
+          inputAffichager.value = "Error";
+          labelHistoryque.textContent = "";
+          return;
+        }
+
+        break;
+      default:
+        break;
+
     }
-    else{
-      labelHistoryque.textContent = labelHistoryque.textContent.slice(0, -1) + ' ' + newOperator;
-      calculEnCours = calculEnCours.slice(0, -1) + ' ' + newOperator;
+
+    if (typeof resultat !== "undefined") {
+      inputAffichager.value = resultat.toString();
+      numeroCourant = resultat.toString();
+      operateurCourant = "";
+      calculEnCours = "";
+
     }
   }
-
-
-  // function operateurs(newOperator) {
-  //   if (numeroCourant !== "") {
-  //     operateurCourant = newOperator;
-  //     calculEnCours += " " + numeroCourant + " " + operateurCourant;
-  //     labelHistoryque.textContent = calculEnCours;
-  //     numeroCourant = "";
-  //   }
-  // }
-
-  division.addEventListener("click", function (event) {
-    event.preventDefault();
-    operateurs("/");
-  });
-
-  multiplication.addEventListener("click", function (event) {
-    event.preventDefault();
-    operateurs("*");
-  });
-
-  soustraction.addEventListener("click", function (event) {
-    event.preventDefault();
-    operateurs("-");
-  });
-
-  addition.addEventListener("click", function (event) {
-    event.preventDefault();
-    operateurs("+");
-  });
-
-  egalites.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (numeroCourant !== "" && operateurCourant !== "") {
-      calculEnCours += numeroCourant;
-      labelHistoryque.textContent = calculEnCours + " = ";
-      let resultat;
-
-      switch (operateurCourant) {
-        case "+":
-        case "-":
-        case "*":
-          resultat = eval(calculEnCours);
-        break;
-          case "/":
-            if (numeroCourant !== "0") {
-              resultat = eval(calculEnCours);
-            } else {
-              inputAffichager.value = "Error";
-              labelHistoryque.textContent = "";
-              return;
-            }
-          
-          break;
-        default:
-          break;
-          
-      }
-
-      if (typeof resultat !== "undefined") {
-        inputAffichager.value = resultat.toString();
-        numeroCourant = resultat.toString();
-        operateurCourant = "";
-        calculEnCours = "";
-        
-      }
-    }
-  });
+});
